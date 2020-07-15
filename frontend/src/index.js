8ded0ad60edda983a1f1a2a095c3b94964ecc6a2
manavi101 <matute50@live.com.ar>
2020-07-15 08:34:02 -0300

    first commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
+
+
+
+
+function Prueba() {
+  return (
+    <div className="prueba">
+      hello
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Prueba />
+    <Hello />
   </React.StrictMode>,
   document.getElementById('root')
 );
