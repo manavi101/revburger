0b0eae43f3b6abc08dd290dc746e75e101fec2e4
manavi101 <matute50@live.com.ar>
2020-08-06 03:02:49 -0300

    Header identation

--- client/src/header/Header.js
+++ client/src/header/Header.js
@@ -9,36 +9,33 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+
 function Header() {  
-  
-
-
   return (
     <div className="Header" >
-    <Grid container alignItems="center" className="Headerpadding" spacing={0} boxShadow={3}>
-      <Grid container item xs={3} spacing={0}>
-        <img src={logo} class="logo" width="55px" height="55px"></img><h3>Revburger</h3>
+      <Grid container alignItems="center" className="Headerpadding" spacing={0} boxShadow={3}>
+        <Grid container item xs={3} spacing={0}>
+          <img src={logo} class="logo" width="55px" height="55px"></img><h3>Revburger</h3>
+        </Grid>
+        <Grid container item xs={6} spacing={0}>
+          <Paper className="SearchbarHeader" component="form"  elevation={2}>
+            <InputBase 
+              className="InputSearchbarHeader"
+              placeholder="Buscar"
+            />
+            <IconButton className="ButtonSearchbarHeader" type="submit"  aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+        </Grid>
+        <Grid container justify="flex-end" item xs={3} spacing={0}>
+          <div style={{paddingRight:"5px"}}>
+            <Button variant="outlined" startIcon={<AccountCircleOutlinedIcon />}>Iniciar Sesion</Button>
+          </div>
+        </Grid>
       </Grid>
-    <Grid container item xs={6} spacing={0}>
-      <Paper className="SearchbarHeader" component="form"  elevation={2}>
-        <InputBase 
-          className="InputSearchbarHeader"
-          placeholder="Buscar"
-        />
-        <IconButton className="ButtonSearchbarHeader" type="submit"  aria-label="search">
-          <SearchIcon />
-        </IconButton>
-      </Paper>
-    </Grid>
-    <Grid container justify="flex-end" item xs={3} spacing={0}>
-    <div style={{paddingRight:"5px"}}>
-      <Button variant="outlined" startIcon={<AccountCircleOutlinedIcon />}>Iniciar Sesion</Button>
     </div>
-    </Grid>
-    
-  </Grid>
-  </div>
-    );
+  );
 }
 
 export default Header;
